a39feaf156beaf07d08bdb3a42d868d8921d1991
mutuotakelvin <kelvinmutuota749@gmail.com>
2023-01-26 23:43:37 +0300

    comparing & hashing password

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,5 +1,14 @@
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
+
+export const comparePasswords = (password, hash) => {
+    return bcrypt.compare(password,hash)
+}
+
+export const hashPassword = (password) => {
+    return bcrypt.hash(password,5)
+}
 
 export const createJWT = (user) => {
     const token = jwt.sign(
