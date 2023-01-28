546e325bcb6aa13820fa1f401d9bda7bef567b5a
mutuotakelvin <kelvinmutuota749@gmail.com>
2023-01-28 12:06:07 +0300

    Adding users route

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,5 +1,5 @@
 import prisma from "../db";
-import { createJWT, hashPassword } from "../modules/auth";
+import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
 
 export const createNewUser = async (req,res) =>{
     const user = await prisma.user.create({
@@ -8,6 +8,25 @@ export const createNewUser = async (req,res) =>{
             password: await hashPassword(req.body.password)
         }
     })
+
+    const token = createJWT(user)
+    res.json({token})
+}
+
+export const signin = async(req,res) => {
+    const user = await prisma.user.findUnique({
+        where:{
+            username: req.body.username
+        }
+    })
+
+    const isValid = await comparePasswords(req.body.password, user.password)
+
+    if(!isValid){
+        res.status(401)
+        res.json({message:'nope'})
+        return
+    }
 
     const token = createJWT(user)
     res.json({token})
--- src/server.ts
+++ src/server.ts
@@ -3,32 +3,16 @@ import router from './router'
 import morgan from 'morgan'
 import cors from 'cors'
 import { protect } from './modules/auth'
+import { createNewUser, signin } from './handlers/user'
 
 const app = express()
 
 app.use(cors())
-
-const customLogger = (message) => (req,res,next) => {
-    console.log(`Hello from ${message}`)
-}
-
-
 app.use(morgan('dev'))
-
-// Allows a client to sent json
 app.use(express.json())
-
-// allows a client to add things like query strings and parameter
-// and it decodes and encodes properly
 app.use(express.urlencoded({extended:true}))
-app.use(customLogger('custom logger'))
 
 
-// costom middleware
-app.use((req,res,next)=>{
-    req.sssh_secret = 'doggy'
-    next()
-})
 app.get('/',(req,res)=>{
     console.log('hello from express')
     res.status(200)
@@ -37,4 +21,6 @@ app.get('/',(req,res)=>{
 
 app.use('/api', protect, router)
 
+app.post('/user',createNewUser)
+app.post('/signin',signin)
 export default app
