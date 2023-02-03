0ed58df41d8046211e4d2ef77d62c80a903e0a48
mutuotakelvin <kelvinmutuota749@gmail.com>
2023-02-03 05:39:36 +0300

    create&update route handlers

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -28,3 +28,47 @@ export const getOneProduct = async (req,res) =>{
 
     res.json({data: product})
 }
+
+// Create product
+export const createProduct = async (req,res) => {
+    const product = await prisma.product.create({
+        data:{
+            name: req.body.name,
+            belongsToId: req.user.id
+        }
+    })
+
+    res.json({data:product})
+}
+
+// Update product
+
+export const updateProduct = async (req,res) => {
+    const updated = await prisma.product.update({
+        where:{
+            id_belongsToId:{
+                id:req.params.id,
+                belongsToId: req.user.id
+            }
+        },
+        data:{
+            name: req.body.name
+        }
+    })
+
+    res.json({data:updated})
+}
+
+// delete product
+export const deleteProduct = async (req,res) => {
+    const deleted = await prisma.product.delete({
+        where:{
+            id_belongsToId:{
+                id:req.params.id,
+                belongsToId: req.user.id
+            }
+        }
+    })
+
+    res.json({data: deleted})
+}
