5f1a6c543955e33c13eabf2966be8c7c00be8011
mutuotakelvin <kelvinmutuota749@gmail.com>
2023-02-03 04:52:43 +0300

    Route Validation

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express'
-import { body, validationResult } from "express-validator";
+import { body, oneOf, validationResult } from "express-validator";
+import { handleInputErrors } from './modules/middleware';
 
 
 const router = Router()
@@ -9,30 +10,42 @@ router.get('/product', (req,res) => {
     res.json({message:"hello"})
 })
 router.get('/product/:id', () => {})
-router.put('/product/:id', body('name').isString(), (req,res) => {
-    const errors = validationResult(req)
-    console.log(errors)
-    
-    if(!errors.isEmpty()){
-        res.status(400)
-        res.json({errors:errors.array()})
-    }
+router.put('/product/:id', body('name').isString(),handleInputErrors,(req,res) => {
 })
-router.post('/product', () => {})
+router.post('/product',body('name').isString(),handleInputErrors, () => {})
 router.delete('/product', () => {})
 
 //  Update
 router.get('/update', () => {})
 router.get('/update/:id', () => {})
-router.put('/update/:id', () => {})
-router.post('/update', () => {})
+router.put('/update/:id',
+    body('title').optional(),
+    body('body').optional(),
+    body('status').isIn(['IN_PROGRESS','LIVE','DEPRECATED','ARCHIVED']),
+    body('version').optional(),
+    handleInputErrors,
+    (req,res) => {
+})
+router.post('/update',
+    body('title').exists().isString(),
+    body('body').exists().isString(),
+    () => {
+
+})
 router.delete('/update', () => {})
 
 // Update Points
 router.get('/updatepoint', () => {})
 router.get('/updatepoint/:id', () => {})
-router.put('/updatepoint/:id', () => {})
-router.post('/updatepoint', () => {})
+router.put('/updatepoint/:id',
+    body('name').optional().isString(),
+    body('description').optional().isString(),
+() => {})
+router.post('/updatepoint',
+    body('name').optional().isString(),
+    body('description').optional().isString(),
+    body('updateId').exists().isString(),
+    () => {})
 router.delete('/updatepoint', () => {})
 
 export default router
